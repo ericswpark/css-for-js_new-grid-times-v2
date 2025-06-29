d6e6b68eb675b1e2c549c7fcd6c4eca249fe7060
Eric Park <me@ericswpark.com>
2025-06-29 17:58:32 -0400

    Try Exercise 5

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import MaxWidthWrapper from '../MaxWidthWrapper';
 
 import VisuallyHidden from '../VisuallyHidden';
+import { QUERIES } from '../../constants';
 
 const Footer = () => {
   return (
@@ -144,6 +145,16 @@ const TopRow = styled.div`
   font-size: 0.875rem;
   border-bottom: 1px solid var(--color-gray-700);
   padding: 24px 0;
+
+  @media ${QUERIES.tabletAndUp} {
+    flex-direction: row;
+    justify-content: center;
+    align-items: revert;
+  };
+
+  @media ${QUERIES.laptopAndUp} {
+    justify-content: end;
+  };
 `;
 
 const Social = styled.div`
@@ -170,6 +181,16 @@ const MainNavArea = styled.div`
   gap: 32px;
   padding: 32px 0 48px;
   text-align: center;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    text-align: revert;
+  };
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: repeat(4, 1fr);
+  };
 `;
 
 const MainNavHeading = styled.h2`
@@ -196,6 +217,10 @@ const Subfooter = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media ${QUERIES.laptopAndUp} {
+    align-items: start;
+  }
 `;
 
 const Logo = styled.a`
